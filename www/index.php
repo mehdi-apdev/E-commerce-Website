ed57c09ea19f10ea61ff5836a143e540a9104e79
mehdi-apdev <mehdi.elmestari.pro@gmail.com>
2025-04-21 12:31:27 +0200

    Debug: Ajout d’un try/catch robuste avec log dans getAllJson() pour diagnostiquer erreur 500 en production

--- www/index.php
+++ www/index.php
@@ -4,17 +4,11 @@
 // 1. Démarrer la session
 session_start();
 
-// 1. Configs
-require_once dirname(__DIR__) . '/config/config.php';
-
-// 2. Charge les helpers AVANT database (car contient env())
-require_once dirname(__DIR__) . '/app/core/helpers.php';
-
-// 3. Autoloader
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-// 4. Connexion DB
-require_once dirname(__DIR__) . '/config/database.php';
+// 2. Configs + autoload + helpers
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../app/core/helpers.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/database.php';
 
 // 3. Nettoyage et parsing de l’URL
 $uri = $_SERVER['REQUEST_URI'];
@@ -22,12 +16,9 @@ $scriptName = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/'); // souvent vide en p
 $relativeUri = '/' . ltrim(str_replace($scriptName, '', $uri), '/');
 $cleanUri = strtok($relativeUri, '?');
 
-// Debug (à enlever en prod)
-file_put_contents(dirname(__DIR__) . '/debug.log', "Entrée dans index.php\nURI nettoyée : $cleanUri\n", FILE_APPEND);
-
 // 4. Routage API
 if (preg_match('#^/api/#', $cleanUri)) {
-    require_once dirname(__DIR__) . '/routes/api.php';
+    require_once __DIR__ . '/../routes/api.php';
     exit;
 }
 
